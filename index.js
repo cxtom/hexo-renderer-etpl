45ae0871f4d955e9912843f0dc7d190cc924b11d
ChenXiao <chenxiao07@baidu.com>
2015-07-24 15:16:59 +0800

    up

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ var _    = require('underscore');
 /* global hexo */
 
 hexo.extend.renderer.register('etpl', 'html', function (data, locals) {
-    return etpl.render(
-                data.text,
+    var renderer = etpl.compile(data.text);
+    return renderer(
                 _.extend({
                     filename: data.path
                 }, locals)
